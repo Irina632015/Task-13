3f09d8f29cbd355a42cd21e1d84ef58ccd8904f8
Irina632015 <irina.632015@yandex.ru>
2023-01-26 23:11:23 +0400

    Семинар 2 Задача 13

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+///645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+int num = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(num)-2;
+Console.WriteLine(k < 0 ? "No such digit" : (num % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+Console.ReadLine();
